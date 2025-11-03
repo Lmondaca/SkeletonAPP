0fae145eb50861e70406bea50744995e8e300534
Lucas Mondaca <lucas.mondaca@hotmail.com>
2025-11-03 20:08:36 -0300

    Se agrega MatDatepicker formativa2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { HomePageRoutingModule } from './home-routing.module';
+
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 
 @NgModule({
+  providers: [provideNativeDateAdapter()],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    
   ],
   declarations: [HomePage]
 })
